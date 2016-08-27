c526d22f9783e56e675826e7b6bd6110d16f605e
Awesome Blog <sumanda.mn@gmail.com>
2016-08-27 10:40:25 +0530

    Fixing Errors

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
-    
+    root 'pages#home'
+    get  'about' => 'pages#about'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
@@ -13,7 +14,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
-
+      resources :articles
   # Example resource route with options:
   #   resources :products do
   #     member do
