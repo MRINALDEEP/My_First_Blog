0741254d948de3741ba8caadc1fd2a3799753e23
Awesome Blog <sumanda.mn@gmail.com>
2016-08-27 19:57:24 +0530

    gkkk

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,7 +13,17 @@ class UsersController < ApplicationController
     end
   end 
   def edit
+    @user=User.find(params[:id])
     
+  end
+  def update
+    
+    if @user.update(user_params)
+      flash[:success]="User Successfully Updated"
+      redirect_to articles_path(@article)
+    else
+      render 'edit'
+    end
   end
   private
     def user_params
