961158208e75549e5727b6a8fdfc74f6c5955025
Awesome Blog <sumanda.mn@gmail.com>
2016-08-27 16:58:56 +0530

    done creating add user_id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-  belongs_to:user
+  belongs_to :user
   validates :title,presence: true,length: {minimum:4,maximum:40}
   validates :description,presence: true,length: {minimum:10,maximum:100}
+  validates :user_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  has_many:articles
+  has_many :articles
   before_save {self.email=email.downcase}
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :username,presence: true,length: {minimum:4,maximum:40},uniqueness: {case_sensitive: false}
