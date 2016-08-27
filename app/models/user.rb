725ec8b21a0402b0c4c07ad4e3b28a4f98c36d74
Awesome Blog <sumanda.mn@gmail.com>
2016-08-27 17:34:37 +0530

    done password Secure

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ActiveRecord::Base
   VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :username,presence: true,length: {minimum:4,maximum:40},uniqueness: {case_sensitive: false}
   validates :email,presence: true,length: {minimum:5,maximum:80},uniqueness: true,format: {with: VALID_EMAIL_REGEX}
+  has_secure_password
 end
