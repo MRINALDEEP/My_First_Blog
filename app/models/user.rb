73406a4c0265a6a1d9660e8e74bd74641f74c413
Awesome Blog <sumanda.mn@gmail.com>
2016-08-27 16:34:19 +0530

    user creation and validation

---
+++ app/models/user.rb
@@ -0,0 +1,7 @@
+class User < ActiveRecord::Base
+  has_many:articles
+  before_save {self.email=email.downcase}
+  VALID_EMAIL_REGEX= /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+  validates :username,presence: true,length: {minimum:4,maximum:40},uniqueness: {case_sensitive: false}
+  validates :email,presence: true,length: {minimum:5,maximum:80},uniqueness: true,format: {with: VALID_EMAIL_REGEX}
+end
