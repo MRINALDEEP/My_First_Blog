37ca04d959117579d2e3c21c13b435885c68f057
Awesome Blog <sumanda.mn@gmail.com>
2016-08-28 23:26:41 +0530

    sign in upon sign up

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,7 @@
 class ArticlesController < ApplicationController
   before_action :set_params, only: [:edit,:show,:update,:destroy]
-  before_action :current_user, except:[:index,:show]
+  before_action :require_user,except: [:index, :show]
+  before_action :require_same_user,only: [:edit,:update,:destroy]
   def index
     @articles=Article.paginate(page: params[:page],per_page:5)
   end
@@ -11,7 +12,7 @@ class ArticlesController < ApplicationController
   def create
    #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user=User.last
+    @article.user=current_user
     
     if @article.save
       flash[:success] = "Article Successfully Created"
@@ -48,4 +49,11 @@ class ArticlesController < ApplicationController
     def article_params
       params.require(:article).permit(:title, :description) 
     end
-end 
+    def require_same_user
+      if current_user != @article.user
+        flash[:danger]="You Can Only Edit Or Delete Your Own Article"
+        redirect_to articles_path
+      end
+    end
+        
+   end 
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :set_users, only: [:edit,:show,:update]
+  before_action :require_same_user,only: [:edit,:show,:update]
   def index
     @users=User.paginate(page: params[:page],per_page:5)
   end
@@ -11,18 +13,19 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
+      session[:user_id]=@user.id
       flash[:success] = "Welcome To The AWSME Blog #{@user.username}"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render 'new'
     end
   end 
   def edit
-    @user=User.find(params[:id])
+   
     
   end
   def update
-    @user=User.find(params[:id])
+    
     if @user.update(user_params)
       flash[:success]="User Successfully Updated"
       redirect_to articles_path(@article)
@@ -32,12 +35,21 @@ class UsersController < ApplicationController
   end
   
   def show
-    @user=User.find(params[:id])
     @user_articles=@user.articles.paginate(page: params[:page],per_page: 5)
   end
   private
+    def set_users
+      @user=User.find(params[:id])
+    end
     def user_params
       params.require(:user).permit(:username, :email, :password) 
+    end
+    
+    def require_same_user
+      if current_user !=@user
+        flash[:danger]="You Are Not Authorized To Do That"
+        redirect_to root_path
+      end
     end
  
 end
